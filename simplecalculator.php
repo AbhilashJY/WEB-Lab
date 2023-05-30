1ce7f979fb512b366d680b50285676f33550349c
Thejaswi <salianthejaswi@gmail.com>
2023-05-30 16:29:47 +0530

    new

--- simplecalculator.php
+++ simplecalculator.php
@@ -1,12 +1,28 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Calculator</title>
 </head>
 <body>
-    <h1></h1>
+    <h1>Simple Calculator Using PHP</h1>
+    <form action="simplecalculator2.php" method="post">
+        <table>
+            <tr><td>Enter First Number:</td><td><input type="text" name="first" required autocomplete="off"/></td></tr>
+            <tr><td>Enter Second Number:</td><td><input type="text" name="second" required autocomplete="off"/></td></tr>
+            <tr><td>Select Operator:</td><td>
+            <select name="op">
+                <option>Select Operation</option>
+                <option value="+">Addition</option>
+                <option value="-">Subtraction</option>
+                <option value="*">Multiplication</option>
+                <option value="/">Division</option>
+                <option value="%">Remainder</option>
+            </select>
+            </td></tr>
+            <tr><td colspan="2"><input type="submit" name="pop" value="PerformOperation"/></td></tr>
+        </table>
+    </form>
+    
 </body>
 </html>
